95b495a518c8fa86f93ad753d482e943c8a66ed9
karinaaduu@gmail.com <karinaaduu@gmail.com>
2020-10-27 04:59:19 +0300

    Добавьте файлы проекта.

---
+++ secondlabc.cpp
@@ -0,0 +1,25 @@
+﻿#include<cmath>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	float x, y;
+	cout << "Введите координаты точки\n";
+	cin >> x >> y;
+	if ((y >= 0) && (y <= 1) && (x >= -1) && (x <= 1))
+	{
+		if ((y - abs(x)) >= 0)
+			cout << "Данная точка принадлежит заштрихованной части\n";
+		else
+			cout << "Данная точка не принадлежит заштрихованной части\n";
+	}
+else
+		cout << "Данная точка не принадлежит заштрихованной части\n";
+	system("pause");
+	return 0;
+
+
+}
+
